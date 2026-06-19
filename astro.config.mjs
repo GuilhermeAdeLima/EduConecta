35c1bc1d9d6cbc3a67083f4fb0a9be7c1000186d
GuilhermeAdeLima <ga.lima1999@hotmail.com>
2026-06-19 00:23:34 -0300

    feat: add sitemap integration and site URL configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  integrations: [tailwind()],
+ site: 'https://guilhermeadelima.github.io',
+ base: '/EduConecta',
+ integrations: [
+    tailwind(),
+    sitemap(),
+  ],
   output: 'static',
-  build: {
-    assets: 'assets',
-  },
 });
